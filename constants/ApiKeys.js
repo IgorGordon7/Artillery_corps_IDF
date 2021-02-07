a34de13ed7b27bb894da4aa6ba2b71ea65a0e549
Igorg Gordon <berkut1987@gmail.com>
2021-02-07 13:08:33 +0200

    Upload my project

---
+++ constants/ApiKeys.js
@@ -0,0 +1,10 @@
+export default {
+    FirebaseConfig: {
+        apiKey: "AIzaSyDp5oLsG37YxljZxQgOHPscDw0ejA5oBmU",
+        authDomain: "testexpo1-fc19c.firebaseapp.com",
+        databaseURL: "https://testexpo1-fc19c.firebaseio.com",
+        projectId: "testexpo1-fc19c",
+        storageBucket: "testexpo1-fc19c.appspot.com",
+        messagingSenderId: "365931918439"
+    }
+}
---
+++ screens/OperationScreen.js
@@ -0,0 +1,32 @@
+
+import React from 'react';
+import {StyleSheet,View,Text,TouchableOpacity} from 'react-native';
+
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import AddOperation from '../screens/operations/AddOperation' 
+import ShowOperation from '../screens/operations/ShowOperation'
+
+const OperationNavigator = createBottomTabNavigator(
+    {
+        תצוגות: {  screen: ShowOperation  },
+    משימות: {  screen: AddOperation  }
+    },{
+        swipeEnabled:false,
+        animationEnabled: false,
+        tabBarOptions: {
+            activeTintColor: 'white',
+    
+            labelStyle: {
+                fontSize: 16,
+            },
+            style: {
+                backgroundColor: '#1c313a',
+            },
+    }
+        
+        
+    });
+
+
+
+export default createAppContainer(OperationNavigator);
